99fef0eac26d447f43cea4cb20fc46c64f922cca
James Royer <walkingalchemy@gmail.com>
2017-10-11 15:42:22 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,40 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |win|
+    if (board[win[0]] == "X" && board[win[1]] == "X" && board[win[2]] == "X") ||
+       (board[win[0]] == "O" && board[win[1]] == "O" && board[win[2]] == "O")
+      return win
+    end
+  end
+  return false
+end
+
+def full?(board)
+  board.all? do |position|
+    position == "X" || position == "O" ?
+    true : false
+  end
+end
+
+def draw?(board)
+ # (won?(board) && !full?(board))
+ if won?(board)
+   return false
+ elsif !full?(board)
+   return false
+ else
+   return true
+end
