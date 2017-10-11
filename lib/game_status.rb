78bd841ddaeee0bdc9f4eb267a0f69737c53686e
James Royer <walkingalchemy@gmail.com>
2017-10-11 15:51:41 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -41,7 +41,8 @@ def over?(board)
 end
 
 def winner(board)
-  if won?(board)
-    return board[won?(board)[0]]
-  end
+  # if won?(board)
+  #   return board[won?(board)[0]]
+  # end
+  board[won?(board)[0]]
 end
